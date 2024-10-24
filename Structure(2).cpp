792542e5a32d3d0c8936168fbb46574a30ddbc98
Tejas Ashok Bhujade <tejas.bhujade24@sanjivani.edu.in>
2024-10-24 10:49:42 +0530

    Add files via upload

---
+++ Structure(2).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Data {
+    int id;
+    char name[50];
+};
+
+int main() {
+    Data d2;
+    
+    ifstream inFile("data.bin", ios::binary); // Open binary file for reading
+    if (inFile.is_open()) {
+        inFile.read(reinterpret_cast<char*>(&d2), sizeof(d2)); // Read binary data
+        inFile.close();
+        cout << "Data read from binary file: ID=" << d2.id << ", Name = " << d2.name << endl;
+    } else {
+        cout << "Error: Could not open file." << endl;
+    }
+    
+    return 0;
+}
